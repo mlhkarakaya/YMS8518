79e20525f9cace7abf007c20dc7caa35b78c171a
Melih Karakaya <melihkarakaya322@gmail.com>
2019-11-24 17:45:05 +0300

    JavaScript çıktı #2

---
+++ JavaScript/JavaScript/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace JavaScript.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Javascript1
+            ()
+        {
+            return View();
+        }
+    }
+}
--- JavaScript/JavaScript/Startup.cs
+++ JavaScript/JavaScript/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace jAVAsCRİPT
+namespace JavaScript
 {
     public class Startup
     {
